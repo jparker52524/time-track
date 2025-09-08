38ce3defe118c60af9bdb5692805af5a1cc910be
Josh Parker <joshuadparker23@gmail.com>
2025-09-07 21:31:56 -0400

    added adjustable navbar height

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,21 +1,27 @@
 import "./NavBar.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
 
 function NavBar() {
-
     const navigate = useNavigate();
+    const location = useLocation();
 
     function handleLogoutClick() {
         localStorage.removeItem("token");
         navigate("/");
     }
 
+    const showBackButton = location.pathname !== "/JobsListPage"
+
     return <>
         <div className="navbar-container">
             <div className="navbar-btn-container">
                   <div className="btn-container">
-                    <button className="back-btn" onClick={() => navigate("/JobsListPage")}>Back</button>
+                    {showBackButton && (
+                        <button className="back-btn" onClick={() => navigate("/JobsListPage")}>
+                        Back
+                        </button>
+                    )}
                  </div>
                  <button className="logout-btn" onClick={handleLogoutClick}>
                     <span className="logout-text">LOGOUT</span>
